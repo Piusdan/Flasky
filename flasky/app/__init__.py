f4ed56aa3e9540136a0237768b2bcc692a945f3c
Piusdan <npiusdan@gmail.com>
2017-01-13 16:05:04 +0300

    Initial commit author: Pius Dan Nyongesa

---
+++ flasky/app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import bootstrap
+from flask.ext.mail import mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy 
+from config import config
