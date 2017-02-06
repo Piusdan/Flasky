d94ef74850d0887cbfe78dc80399ba0c525fa5ae
Piusdan <npiusdan@gmail.com>
2017-02-06 17:14:58 +0300

    final changes

--- flasky/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-from flask.ext.bootstrap import bootstrap
-from flask.ext.mail import mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy 
-from config import config
