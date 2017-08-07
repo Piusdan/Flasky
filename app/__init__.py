11b6e2d5486431ef7c4205ff1ec087e20311d10a
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-08-07 16:29:01 +0300

    more changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mail import Mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy 
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from config import config
-from flask.ext.login import LoginManager
-from flask.ext.pagedown import PageDown
+from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -22,6 +22,10 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
 
     pagedown.init_app(app)
     bootstrap.init_app(app)
